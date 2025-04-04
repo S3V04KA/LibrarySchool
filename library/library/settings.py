682d2722b55db9f19271368e4399d595172f72ba
S3V04KA <sevstar2206@gmail.com>
2025-04-05 00:13:49 +0500

    fix

--- library/library/settings.py
+++ library/library/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+STATIC_ROOT = os.environ.get('STATIC_ROOT', '/app/staticfiles')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
